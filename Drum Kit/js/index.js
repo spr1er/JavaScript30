e1ad21ecc4a4a2114575c39944b2beb29605a0c6
spr1er <gg_von1@outlook.com>
2026-07-03 17:44:55 +0300

    feat: update index.js

--- Drum Kit/js/index.js
+++ Drum Kit/js/index.js
@@ -1,2 +1,57 @@
-console.log('Loading...');
-console.log('App start');
+const BUTTONS = [
+  {
+    title: "A",
+    text: "sound 1",
+    urlSound: "./assets/sounds/boom.wav",
+  },
+  {
+    title: "B",
+    text: "sound 2",
+    urlSound: "./assets/sounds/clap.wav",
+  },
+  {
+    title: "C",
+    text: "sound 3",
+    urlSound: "./assets/sounds/hihat.wav",
+  },
+];
+
+const drawButton = (button) => {
+  return `<li class="drum__item flex">
+						<button class="drum__item-btn flex">
+							<h2 class="drum__item-title">${button.title}</h2>
+							<p class="drum__item-paragraph">${button.text}</p>
+						</button>
+					</li>`;
+};
+
+const drawInterface = () => {
+  const btnList = document.querySelector("#drum-list");
+  const template = document.createElement("template");
+
+  for (const btn of BUTTONS) {
+    template.innerHTML = drawButton(btn).trim();
+    createButton(template.content, btn.urlSound);
+    btnList.appendChild(template.content);
+  }
+};
+
+const createButton = (template, sound) => {
+  const audio = new Audio(sound);
+  const button = template.querySelector(".drum__item-btn");
+  button.addEventListener("click", () => {
+    audio.volume = 0.5;
+    audio.currentTime = 0;
+  audio.play();
+  });
+};
+
+document.addEventListener('keydown', () => {
+	
+});
+
+const main = () => {
+  drawInterface();
+};
+
+main();
